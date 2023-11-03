0d833f8d01cf022efb11896c93b2487f04c48d0b
jackroberts1993 <jackroberts1993@gmail.com>
2023-11-03 13:46:48 +0000

    SpringBoot Initialiser - First Commit

---
+++ src/main/java/com/example/jackspetitions/JackspetitionsApplication.java
@@ -0,0 +1,13 @@
+package com.example.jackspetitions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JackspetitionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JackspetitionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jackspetitions/JackspetitionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jackspetitions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JackspetitionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
