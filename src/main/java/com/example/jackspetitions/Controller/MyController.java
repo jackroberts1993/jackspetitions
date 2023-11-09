28e4d33b13d41ebc1bd9062d035e71a2c92b3d54
jackroberts1993 <jackroberts1993@gmail.com>
2023-11-09 15:03:15 +0000

    1. Refactered Controller. Mapped search get and post 2. Created search.html page using thymeleaf notation

--- src/main/java/com/example/jackspetitions/Controller/MyController.java
+++ src/main/java/com/example/jackspetitions/Controller/MyController.java
@@ -1,18 +1,23 @@
 package com.example.jackspetitions.Controller;
 
 import com.example.jackspetitions.Model.Petition;
+import com.example.jackspetitions.Model.Search;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.HashMap;
+import java.util.Map;
 
 
 @Controller
 public class MyController {
 
     private HashMap<Integer,Petition> petitionMap = new HashMap<>();
+
+    private HashMap<Integer,Petition> searchMap = new HashMap<>();
+    private Search search = new Search();
 
     @GetMapping("/")
     public String index(Model model) {
@@ -38,6 +43,21 @@ public class MyController {
         // You can add any necessary model attributes here if needed
         model.addAttribute("petitionMap", petitionMap);
         return "allpetitions";
+    }
+
+    @PostMapping("/search")
+    public String searchPetitions(Search search) {
+        // Search through the petitionList for any petitions that contain the search string
+        // Add the matching petitions to the model
+        searchMap.clear();
+        for (Map.Entry<Integer, Petition> entry: petitionMap.entrySet()) {
+            if (entry.getValue().getTitle().contains(search.getSearch()) || entry.getValue().getDescription().contains(search.getSearch()) || entry.getValue().getAuthor().contains(search.getSearch())) {
+                searchMap.put(entry.getKey(), entry.getValue());
+                System.out.println(entry.getValue().getTitle());
+                System.out.println(search.getSearch());
+            }
+        }
+        return "redirect:/search";
     }
 
 }
