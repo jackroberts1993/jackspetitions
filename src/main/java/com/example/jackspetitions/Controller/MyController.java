f4f6c193550bcf25831463651dd5893c6492d38e
jackroberts1993 <jackroberts1993@gmail.com>
2023-11-09 14:59:03 +0000

    1. Refactered Controller. Mapped homepage to index.html 2. Created create.html page using thymeleaf notation

--- src/main/java/com/example/jackspetitions/Controller/MyController.java
+++ src/main/java/com/example/jackspetitions/Controller/MyController.java
@@ -1,18 +1,36 @@
 package com.example.jackspetitions.Controller;
 
+import com.example.jackspetitions.Model.Petition;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.HashMap;
 
 
 @Controller
 public class MyController {
 
+    private HashMap<Integer,Petition> petitionMap = new HashMap<>();
+
     @GetMapping("/")
     public String index(Model model) {
         // You can add any necessary model attributes here if needed
         return "index";
+    }
+
+    @GetMapping("/create")
+    public String create(Model model) {
+        // You can add any necessary model attributes here if needed
+        model.addAttribute("petition", new Petition());
+        return "create";
+    }
+
+    @PostMapping("/create")
+    public String createPetition(Petition petition) {
+        petitionMap.put(petitionMap.size() + 1, petition);
+        return "create";
     }
 
 }
